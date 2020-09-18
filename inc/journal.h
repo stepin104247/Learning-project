f63de5f8c3051e466d0298bdc3e75854e2090f16
stepin104247 <69038461+stepin104247@users.noreply.github.com>
2020-09-19 01:59:54 +0530

    Create journal.h

---
+++ inc/journal.h
@@ -0,0 +1,14 @@
+#ifndef __JOURNAL_H__
+#define __JOURNAL_H__
+
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+#include<stdlib.h>
+
+void addtodo();
+void viewtodo();
+void mark_done();
+void delete_todo();
+
+#endif 
