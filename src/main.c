a558467b2d2094eafba03f9310362277a398ff2d
stepin104247 <69038461+stepin104247@users.noreply.github.com>
2020-09-19 10:16:20 +0530

    Update main.c

--- src/main.c
+++ src/main.c
@@ -5,7 +5,8 @@
 #include <string.h>
 #include<stdlib.h>
 
-
+?
+    /*Structure storing the details of the todo list. Status shows whether the task is done or not*/
 struct todos
 {
     char taskname[35];
@@ -17,25 +18,27 @@ struct todos
 int main()
 { 
 int option;
+    /*displays the main menu*/
 while(1){
   printf("\n\n****************JOURNAL-TO DOs****************\n\n\n\n");
   printf("               ****MENU****\n");
   printf("\nADD TO-DO         [1]\nVIEW TO-DO        [2]\nMARK DONE         [3]\nDELETE TO-DO      [4]\nEXIT              [5]\n\n\nENTER YOUR CHOICE: ");
   scanf("%d",&option);
 
+    
   switch (option)
   {
   case (1):
-      addtodo();
+      addtodo(); /*adds a to do list to the file with the name of the date mentioned*/
       break;
   case(2):
-      viewtodo();
+      viewtodo();/*function that allows to view the to do list of the specified date*/
       break;
   case(3):
-      mark_done();
+      mark_done(); /*function to mark a task as done or undone*/
       break;
   case(4):
-      delete_todo();
+      delete_todo(); /*to delete a particular task from to do list*/
       break;
 
   case(5):
@@ -44,7 +47,7 @@ while(1){
        exit(0);
   
   default:
-      printf("\nPlease enter valid option(1-6)");
+      printf("\nPlease enter valid option(1-6)"); /*Input the number to choose the menu option*/
       printf("\n Press any key to retry");
       getch();
       fflush(stdin);
